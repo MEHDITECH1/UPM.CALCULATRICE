e05aaaa9650eea45fe3eba0eebc2f7f2ca2a7adb
MEHDITECH1 <96782117+MEHDITECH1@users.noreply.github.com>
2022-01-07 09:40:22 +0100

    Add files via upload

---
+++ MA_CALCULATRICE2/src/ma_calculatrice2/MA_CALCULATRICE2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ma_calculatrice2;
+
+/**
+ *
+ * @author MR UNLOCKER
+ */
+public class MA_CALCULATRICE2 {
+
+   
+    public static void main(String[] args) {
+        new Calc().setVisible(true);
+    }
+    
+}
